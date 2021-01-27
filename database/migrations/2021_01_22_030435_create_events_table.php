f38ec7633dfb00b2ec0a00149d5ee926c996a485
peeraphut <p.peeraphut@gmail.com>
2021-01-27 10:24:28 +0700

    Start install project

---
+++ database/migrations/2021_01_22_030435_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->datetime('start');
+            $table->datetime('end');
+            $table->string('color', 7);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
